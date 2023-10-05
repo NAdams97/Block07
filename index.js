e7e5a62d910280b7e68b59a9d680ec2d8420be6e
NAdams97 <nitoad97@gmail.com>
2023-10-05 14:16:25 -0400

    added files

---
+++ index.js
@@ -0,0 +1,4 @@
+let codeOne = 22%12;
+let codeTwo = 10*4;
+let codeThree = 40-1;
+alert("You have received this message because you have been chosen to open an important vault. Here is the secret combination: " + codeOne + "-" + codeTwo + "-" + codeThree + ".");
