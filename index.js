f05b513bb8f15fc1587d01a4174b4dac57d4c157
NAdams97 <nitoad97@gmail.com>
2023-10-05 14:27:36 -0400

    added pseudocode to js file

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+// Pseudocode: Create a variable for the first value of 10 by dividing 22 by 10 using modulus to get a remainder of 10.
 let codeOne = 22%12;
+// Pseudocode: Create a variable for the second value by multiplying the result of the first vairable by 4.
 let codeTwo = 10*4;
+// Pseudocode: Create a variable for the third value by subtracting 1 from the second variable.
 let codeThree = 40-1;
+// Pseudocode: Create an alert to be displayed by a code call in the html to display the required text as well as the variables in the correct order.
 alert("You have received this message because you have been chosen to open an important vault. Here is the secret combination: " + codeOne + "-" + codeTwo + "-" + codeThree + ".");
